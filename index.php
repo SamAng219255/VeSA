410714ff2933f9ab3a9b71ae8494ec848c431a74
SamAng219255 <SamAng219255@gmail.com>
2018-04-11 11:22:33 -0700

    updated

--- index.php
+++ index.php
@@ -5,12 +5,20 @@
 	<link href="./img/icon.png" rel="shortcut icon">
 </head>
 <body>
-	<img id="background" src="./img/Veil_Nebula.jpg"></img>
-	<div class="txtblock">
-		<h1>Venusian Space Agency</h1>
-	</div>
-	<div class="txtblock">
-		<p>Hallo</p>
+	<div id="parallax">
+		<div id="background"></div>
+		<div id="topbar">
+			<img id="titlelogo" src="./img/VeSA.png">
+			<h1>The Venusian<br>Space Agency</h1>
+		</div>
+		<div class="txtblock">
+			<h3>Some Title Here.</h3>
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+			<img src="./img/VeSA.png">
+		</div>
+		<div class="txtblock"><h3>Some Title Here.</h3></div>
+		<div class="txtblock"><h3>Some Title Here.</h3></div>
+		<img src="./img/Veil_Nebula_long.jpg" width="100%">
 	</div>
 </body>
 </html>
