655e6a4f9310b10e58b7d962d161921ce636471b
SamAng219255 <SamAng219255@gmail.com>
2018-04-12 11:21:06 -0700

    updated

--- index.php
+++ index.php
@@ -16,9 +16,99 @@
 			<p>Hi! we're a private space agency based out of orbit around Venus. We give rides to scientists and tourists into extra-solar space, though our goal is to establish a permanent connection between Our solar system and the next one over.</p>
 			<p>Along this line we are researching negative matter stabilized worm holes and controlled mass quantum tunneling technologies which will allow us to create a sort of bridge between any two points in space and travel there quickly without requiring FTL (Faster Than Light) engines.</p>
 			<p>Sorry for the science jargon... this is a complex procedure. Basically we are punching holes in space which create and connect to other holes in space. While the connection between these holes is much shorter than the normal travel distance, our bodies don't really like fitting around the edges of the holes and you'd need to lose quite a bit of weight to fit through given the holes are smaller than anyone can see. That's where the tunneling comes into play. You see quantum tunneling is a strange phenomenon, the best analogy would be if you got locked up in solitary confinement and then suddenly found yourself outside the prison. We plan to use this strange trick to sort of teleport through the wormhole avoiding all the messiness involved in actually going through one.</p>
+			<img class="centeredimg" src="./img/wormhole.gif">
 		</div>
-		<div class="txtblock"><h3>Some Title Here.</h3></div>
-		<div class="txtblock"><h3>Some Title Here.</h3></div>
+		<div class="txtblock">
+			<h3>Computer Bid</h3>
+			<table id="bid">
+				<tr>
+					<th>Category</th>
+					<th>Item</th>
+					<th>Additional Info.</th>
+					<th>Price per Item</th>
+					<th>Qty.</th>
+					<th>Cost</th>
+				</tr>
+				<tr>
+					<td>Client Machine</td>
+					<td>iMac Pro</td>
+					<td></td>
+					<td>$5,000</td>
+					<td>20</td>
+					<td>$100,000</td>
+				</tr>
+				<tr>
+					<td>Server</td>
+					<td></td>
+					<td></td>
+					<td></td>
+					<td>1</td>
+					<td></td>
+				</tr>
+				<tr>
+					<td>Wall Screen</td>
+					<td>InFocus INF6500e</td>
+					<td>JTouch 65” Full HD</td>
+					<td>$2,099</td>
+					<td>1</td>
+					<td>$2,099</td>
+				</tr>
+				<tr>
+					<td>Switch</td>
+					<td>Edgeswitch</td>
+					<td>Layer 3</td>
+					<td>$580</td>
+					<td>1</td>
+					<td>$580</td>
+				</tr>
+					<td>Firewall</td>
+					<td>ASA5506-x</td>
+					<td>FirePOWER Services</td>
+					<td>$680</td>
+					<td>1</td>
+					<td>$680</td>
+				</tr>
+					<td>Router</td>
+					<td>Edgerouter Infinity</td>
+					<td></td>
+					<td>$1,600</td>
+					<td>1</td>
+					<td>$1,600</td>
+				</tr>
+					<td>Cable</td>
+					<td>CAT6</td>
+					<td>Bulk 1,000ft</td>
+					<td>$268</td>
+					<td>N/A</td>
+					<td>$268</td>
+				</tr>
+					<td>Camera</td>
+					<td>GWSecurity</td>
+					<td></td>
+					<td>$4,700</td>
+					<td>32</td>
+					<td>$4,700</td>
+				</tr>
+				<tr>
+					<td colspan="5">Total:</td>
+					<td>$109,926</td>
+				</tr>
+			</table>
+		</div>
+		<div class="txtblock">
+			<h3>Contact Us</h3>
+			<dl>
+				<dt>E-mail</dt>
+				<dd>VenusianSA@gmail.com</dd>
+				<dt>Phone</dt>
+				<dd>2-000-000-425-551-234</dd>
+				<dt>Address</dt>
+				<dd>Sol, Venus, Orbit, (0km, 0km, 39455.435km)</dd>
+				<dt>Hours</dt>
+				<dd>Reply: 2-22 LT, Visit: 6-18 LT</dd>
+			</dl>
+			<img src="./img/VeSA.png" style="position:absolute; top:20px; right:20px;">
+		</div>
 		<img src="./img/Veil_Nebula_long.jpg" width="100%">
 	</div>
 </body>
