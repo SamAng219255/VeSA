d15fcc06bfd446154c9611fd32939cd5454e19d9
sam <SamAng219255@gmail.com>
2018-04-12 11:22:08 -0700

    updated

--- index.php
+++ index.php
@@ -16,6 +16,7 @@
 			<p>Hi! we're a private space agency based out of orbit around Venus. We give rides to scientists and tourists into extra-solar space, though our goal is to establish a permanent connection between Our solar system and the next one over.</p>
 			<p>Along this line we are researching negative matter stabilized worm holes and controlled mass quantum tunneling technologies which will allow us to create a sort of bridge between any two points in space and travel there quickly without requiring FTL (Faster Than Light) engines.</p>
 			<p>Sorry for the science jargon... this is a complex procedure. Basically we are punching holes in space which create and connect to other holes in space. While the connection between these holes is much shorter than the normal travel distance, our bodies don't really like fitting around the edges of the holes and you'd need to lose quite a bit of weight to fit through given the holes are smaller than anyone can see. That's where the tunneling comes into play. You see quantum tunneling is a strange phenomenon, the best analogy would be if you got locked up in solitary confinement and then suddenly found yourself outside the prison. We plan to use this strange trick to sort of teleport through the wormhole avoiding all the messiness involved in actually going through one.</p>
+			<!--<img src="./img/wormhole.gif">-->
 		</div>
 		<div class="txtblock"><h3>Some Title Here.</h3></div>
 		<div class="txtblock"><h3>Some Title Here.</h3></div>
